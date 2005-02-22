0c61835ba857b47a5ffd8c37bdd44b869e1034b8
Martynas Jocius <martynas@idiles.com>
2005-02-22 20:20:31 +0000

    Editor test added.
    git-svn-id: https://svn.code.sf.net/p/opendict/code/trunk@255 ddc53ff1-2c8f-4ce9-baca-bc6ff6589a0e

---
+++ lib/tests/test_editor.py
@@ -0,0 +1,111 @@
+"""
+Unit tests for editor.py
+"""
+
+import unittest
+import os
+import sys
+
+sys.path.append('..')
+
+import dicteditor
+
+
+class TestTranslation(unittest.TestCase):
+    """Translation test"""
+
+    translations = dicteditor.Translation()
+        
+
+    def test_word(self):
+        """Set/Get word should return correct value"""
+
+        self.translations.setWord('test')
+
+        self.assertEquals(self.translations.getWord(), 'test')
+
+
+    def test_translation(self):
+        """Should return correct translations"""
+
+        self.translations.addTranslation('trans1')
+        self.translations.addTranslation('trans2', 'comment2')
+        self.translations.addTranslation('trans3')
+        self.translations.addTranslation('trans3', 'comment3')
+
+        trans = self.translations.getTranslations()
+
+        self.assertEquals(trans.has_key('trans1'), True)
+        self.assertEquals(len(trans.keys()), 3)
+        self.assertEquals(trans['trans3'], 'comment3')
+
+        
+
+class TestEditor(unittest.TestCase):
+    """Editor test"""
+
+    def test_load(self):
+        """Editor should load dictionary from file"""
+
+        editor = dicteditor.Editor()
+        self.assertEquals(len(editor.getUnits()), 0)
+        
+        editor.load("data/sampledict.dwa")
+        self.assertEquals(len(editor.getUnits()), 8)
+
+
+    def test_save(self):
+        """Editor should correctly write dictionary to disk"""
+
+        editor = dicteditor.Editor()
+        editor.load("data/sampledict.dwa")
+        units = editor.getUnits()
+        editor.save("data/__output.dwa")
+        editor.load("data/__output.dwa")
+
+        self.assertEquals(units, editor.getUnits())
+
+        os.unlink("data/__output.dwa")
+
+
+    def test_getUnit(self):
+        """getUnit() should return desired Translation object"""
+
+        editor = dicteditor.Editor()
+        editor.load("data/sampledict.dwa")
+        oldCount = len(editor.getUnits())
+
+        self.assert_(editor.getUnit('du') != None)        
+
+
+    def test_addUnit(self):
+        """addUnit() should add new translation unit or update old one"""
+
+        editor = dicteditor.Editor()
+        editor.load("data/sampledict.dwa")
+        oldCount = len(editor.getUnits())
+
+        newUnit = dicteditor.Translation()
+        newUnit.setWord("test")
+        newUnit.addTranslation("utiutiu")
+
+        editor.addUnit(newUnit)
+
+        self.assertEquals(len(editor.getUnits()), oldCount + 1)
+
+
+    def test_removeUnit(self):
+        """removeUnit() should add new translation unit or update old one"""
+
+        editor = dicteditor.Editor()
+        editor.load("data/sampledict.dwa")
+        oldCount = len(editor.getUnits())
+
+        unit = editor.getUnit('vienas')
+        editor.removeUnit(unit)
+        
+        self.assertEquals(len(editor.getUnits()), oldCount - 1)
+        
+
+if __name__ == "__main__":
+    unittest.main()
