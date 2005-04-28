c82976871a1f560b218e995ae37cb6b58da46d77
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2005-04-28 23:09:58 +0000

    encodings work with py2exe
    git-svn-id: https://svn.code.sf.net/p/opendict/code/trunk@371 ddc53ff1-2c8f-4ce9-baca-bc6ff6589a0e

--- win32/compile.bat
+++ win32/compile.bat
@@ -1,2 +1,2 @@
 rem Copy compile.bat and setup.py to root directory and run compile.bat
-C:\Python24\python.exe setup.py py2exe --includes encodings
+C:\Python24\python.exe setup.py py2exe --packages encodings
