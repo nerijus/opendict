ad46a7610b9600c266b3b1fbeaedc6c2cfc6bfac
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2005-04-29 00:16:51 +0000

    added setup script with manifest
    git-svn-id: https://svn.code.sf.net/p/opendict/code/trunk@373 ddc53ff1-2c8f-4ce9-baca-bc6ff6589a0e

--- win32/compile.bat
+++ win32/compile.bat
@@ -1,3 +1,5 @@
 rem Copy compile.bat and setup.py to root directory and run compile.bat
 C:\Python24\python.exe setup.py py2exe --packages encodings
+rem Use this to get controls with the Windows XP appearance
+rem C:\Python24\python.exe setup.manifest.py py2exe --packages encodings
 copy copying.html dist
---
+++ win32/setup.manifest.py
@@ -0,0 +1,62 @@
+# Copy compile.bat and setup.py to root directory and run compile.bat
+
+from distutils.core import setup
+import py2exe
+import sys
+import glob
+
+# The manifest will be inserted as resource into opendict.exe.  This
+# gives the controls the Windows XP appearance (if run on XP ;-)
+#
+# Another option would be to store if in a file named
+# opendict.exe.manifest, and probably copy it with the data_files
+# option.
+#
+manifest_template = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<assemblyIdentity
+    version="5.0.0.0"
+    processorArchitecture="x86"
+    name="%(prog)s"
+    type="win32"
+/>
+<description>%(prog)s Program</description>
+<dependency>
+    <dependentAssembly>
+        <assemblyIdentity
+            type="win32"
+            name="Microsoft.Windows.Common-Controls"
+            version="6.0.0.0"
+            processorArchitecture="X86"
+            publicKeyToken="6595b64144ccf1df"
+            language="*"
+        />
+    </dependentAssembly>
+</dependency>
+</assembly>
+'''
+
+RT_MANIFEST = 24
+
+################################################################
+# arguments for the setup() call
+
+opendict = dict(
+    script = "opendict.py",
+    other_resources = [(RT_MANIFEST, 1, manifest_template % dict(prog="OpenDict"))],
+    )
+
+options = {"py2exe": {"compressed": 1,
+                      "optimize": 2}}
+
+
+setup(
+    name="OpenDict",
+    version="0.5.8",
+    options = options,
+    zipfile = None,
+    package_dir = {"": "lib"},
+    windows = [opendict],
+    data_files=[("pixmaps", glob.glob("pixmaps/\\*.png"))],
+    )
