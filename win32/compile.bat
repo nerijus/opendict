09f1b1c6508d260a5e8b85128c38547b336f1411
Nerijus Baliūnas <nerijus@users.sourceforge.net>
2005-10-25 21:19:27 +0000

    copy msvcr71.dll
    git-svn-id: https://svn.code.sf.net/p/opendict/code/trunk@397 ddc53ff1-2c8f-4ce9-baca-bc6ff6589a0e

--- win32/compile.bat
+++ win32/compile.bat
@@ -3,3 +3,6 @@ C:\Python24\python.exe setup.py py2exe --packages encodings
 rem Use this to get controls with the Windows XP appearance
 rem C:\Python24\python.exe setup.manifest.py py2exe --packages encodings
 copy copying.html dist
+echo don't forget to copy msvcr71.dll from wxPython/distrib/msw/ to dist
+echo before making a release
+copy C:\Python24\msvcr71.dll dist
